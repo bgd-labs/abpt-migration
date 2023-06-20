a75c98c77b3e92b5897c4f14a310e3f5ea444258
Lukas <lukasstrassel@googlemail.com>
2023-06-20 10:58:09 +0200

    test: redemption

--- tests/E2E.t.sol
+++ tests/E2E.t.sol
@@ -4,16 +4,18 @@ pragma solidity ^0.8.0;
 import 'forge-std/Test.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {AggregatedStakedTokenV3} from 'aave-stk-v1-5/interfaces/AggregatedStakedTokenV3.sol';
+import {IERC20} from 'aave-stk-v1-5/interfaces/IERC20.sol';
 import {DeployPool} from '../scripts/01_DeployPool.s.sol';
 import {DeployImpl} from '../scripts/02_DeployStkAbptV2Impl.sol';
 import {DeployPayload} from '../scripts/03_DeployPayload.sol';
 
 contract E2E is Test {
+  address constant STK_ABPT_WHALE = 0xF23c8539069C471F5C12692a3471C9F4E8B88BC2;
+  address public constant STK_ABPT_V1 = 0xa1116930326D21fB917d5A27F1E9943A9595fb47;
+
   function setUp() external {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 17514427);
-  }
-
-  function testE2E() public {
     DeployPool step1 = new DeployPool();
     (address pool, bytes32 poolId) = step1._deploy();
     DeployImpl step2 = new DeployImpl();
@@ -23,4 +25,21 @@ contract E2E is Test {
 
     GovHelpers.executePayload(vm, payload, AaveGovernanceV2.SHORT_EXECUTOR);
   }
+
+  /**
+   * @dev The proposal upgrades the implementation of stkABPT.
+   * The new version enters post slashing mode, which means ppl should be able to withdraw without a cooldown.
+   */
+  function testRedeem() public {
+    vm.startPrank(STK_ABPT_WHALE);
+    address abpt = AggregatedStakedTokenV3(STK_ABPT_V1).STAKED_TOKEN();
+    uint256 stkAbptBalanceBefore = IERC20(STK_ABPT_V1).balanceOf(STK_ABPT_WHALE);
+    uint256 abptBalanceBefore = IERC20(abpt).balanceOf(STK_ABPT_WHALE);
+    AggregatedStakedTokenV3(STK_ABPT_V1).redeem(address(STK_ABPT_WHALE), stkAbptBalanceBefore);
+
+    assertEq(IERC20(STK_ABPT_V1).balanceOf(STK_ABPT_WHALE), 0);
+    assertEq(IERC20(abpt).balanceOf(STK_ABPT_WHALE), abptBalanceBefore + stkAbptBalanceBefore);
+  }
+
+  function testMigrate() public {}
 }
